12d526d9c93b0ae1b51796ed2a351b10609fde3e
Mustan Rathwala <rathmustan53@gmail.com>
2024-11-04 16:17:32 +0530

    Adding...

---
+++ script.js
@@ -0,0 +1,9 @@
+const sliderTrack = document.querySelector('.slider-track');
+
+sliderTrack.addEventListener('mouseover', () => {
+  sliderTrack.style.animationPlayState = 'paused';
+});
+
+sliderTrack.addEventListener('mouseout', () => {
+  sliderTrack.style.animationPlayState = 'running';
+});
